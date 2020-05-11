f9415fcf9139e5abbb40b5489796919db44fb35c
Hazael Lopez <elhazael134@gmail.com>
2020-05-11 17:14:24 -0600

    Programa simple... Por ser un hola mundo no deja deser un programa en Python

---
+++ Cap01_ATS/HolaMundo.py
@@ -0,0 +1,11 @@
+'''
+    Esto son comentarios multilinea
+    Los comentarios multilinea son bastante utilez
+    más que todo para documentar cuando creamos un clases un cuando creamos un
+    nuevo metodo en el proyecto
+'''
+
+# Estos son comentarios de una sola linea..
+
+print("Hola Mundo!!!!") # Los comentarios de una sola linea se pueden empezar despues de una linea de codigo
+print("Hola Mi sufrido mundo\nYo soy Hazael López")
