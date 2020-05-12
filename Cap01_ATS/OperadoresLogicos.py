c42f3048f61147a6d5fedf3395a9dfe2ae31bb3a
Hazael Lopez <elhazael134@gmail.com>
2020-05-12 09:56:16 -0600

    Solo lo del video

---
+++ Cap01_ATS/OperadoresLogicos.py
@@ -0,0 +1,11 @@
+'''
+    operadores logicos
+    operador And es una multiplicacion logica
+    operador Or es una suma logica
+    Operador Not falso = verdadero y verdadero = falso
+
+    Prioridad
+    1- Not
+    2- And
+    3- OR
+'''
