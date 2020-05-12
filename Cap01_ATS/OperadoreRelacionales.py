5b451cc92a7bc6667d0609750191158ce6bdf38b
Hazael Lopez <elhazael134@gmail.com>
2020-05-12 09:51:29 -0600

    Solo lo del video

---
+++ Cap01_ATS/OperadoreRelacionales.py
@@ -0,0 +1,14 @@
+# Operadores relacionales
+
+'''
+    Veremos los operadores relacionales en Python
+    Los operadores relacionales tiene menos prioridad que los operadores Aritmeticos
+    <   mayor que
+    >   menor que
+    <=  mayor o igual que
+    >=  menor o igual que
+    !=  distinto de
+    =   asignacion
+    ==  igualdad 
+'''
+
