faff580d3c9a69461bdd1e712ebcdc0e902f39be
Hazael Lopez <elhazael134@gmail.com>
2020-05-12 10:04:20 -0600

    Solo lo del video

---
+++ Cap01_ATS/OperadoresAsignacion.py
@@ -0,0 +1,11 @@
+# Operadores de Asignacion
+
+'''
+    Temos que los valores de asignacion son los siguientes
+    += mas igual    -> Suma en asignacion
+    -= menos igual  -> Resta en asignacion
+    *= multiplica igual -> Multiplicación en signacion
+    /= Division igual -> División en asignacion
+    **= Potencia igual -> Potencia en asignacion
+    %= Modulo igual -> Modulo en asignación
+'''
