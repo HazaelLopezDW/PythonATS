fb802ca272c9b7bd161e6cc8215756f78e2d60e0
Hazael Lopez <elhazael134@gmail.com>
2020-05-12 09:22:46 -0600

    Solo vimos los comentarios en Python

---
+++ Cap01_ATS/Comentarios.py
@@ -0,0 +1,9 @@
+'''
+    Comentarios en python, hay dos tipos de comentarios en python
+    Este se prodria describir como un comentario multilinea
+    Estan delimitados por las triples comillas. Tal como este
+'''
+
+# Este es un comentario de una sola linea. O comentarios Simples
+
+print("Comentarios en Python")
