74a114696d7647199352b1f7d0cfcc6978e72a38
Hazael Lopez <elhazael134@gmail.com>
2020-05-12 23:37:12 -0600

    Progama que compra dos numeros con los operadores relacionales

--- Cap01_ATS/OperadoreRelacionales.py
+++ Cap01_ATS/OperadoreRelacionales.py
@@ -12,3 +12,24 @@
     ==  igualdad
 '''
 
+# Haremos un programa que pida al usuario dos numero y los compararemos
+
+primerNumero = int(input("Ingrese el primer numero a comparar "))    # Primer entero introducido por el usuario
+segundoNumero = int(input("Ingrese el segundo numero a comparar "))  # Segundo numero introducido por el usuario
+
+# Hacemos nuestra compracion
+if primerNumero == segundoNumero:
+    print(f"{primerNumero} y {segundoNumero} son iguales")
+if primerNumero > segundoNumero:
+    print(f"{primerNumero} es mayor que {segundoNumero}")
+if primerNumero >= segundoNumero:
+    print(f"{primerNumero} es mayor o igual que {segundoNumero}")
+if primerNumero < segundoNumero:
+    print(f"{primerNumero} es menor que {segundoNumero}")
+if primerNumero <= segundoNumero:
+    print(f"{primerNumero} es menor o igual que {segundoNumero}")
+if primerNumero != segundoNumero:
+    print(f"{primerNumero} y {segundoNumero} son distintos")
+
+
+
