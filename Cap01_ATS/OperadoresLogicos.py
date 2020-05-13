9ffb507a362d86ddfd16e11ecc0d85edac4cb29b
Hazael Lopez <elhazael134@gmail.com>
2020-05-12 23:50:37 -0600

    Progama que muestra las tablas de la verdad de los operadores logicons AND OR NOT

--- Cap01_ATS/OperadoresLogicos.py
+++ Cap01_ATS/OperadoresLogicos.py
@@ -9,3 +9,48 @@
     2- And
     3- OR
 '''
+
+# simplemente mostraremos su tabla de la verdad
+
+mensaje = "TABLAS DE LA VERDADES\n\n"
+verdadero = True
+falso = False
+
+mensaje += "TABLA DE LA VERDAD DEL OPERADOR LOGICO AND\n"
+mensaje += f"True and True -> {verdadero and  verdadero} \n"
+mensaje += f"True and False -> {verdadero and  falso} \n"
+mensaje += f"False and True -> {falso and  verdadero} \n"
+mensaje += f"False and False -> {falso and  falso} \n\n"
+
+mensaje += "TABLA DE LA VERDAD DEL OPERADOR LOGICO OR\n"
+mensaje += f"True and True -> {verdadero or  verdadero} \n"
+mensaje += f"True and False -> {verdadero or  falso} \n"
+mensaje += f"False and True -> {falso or  verdadero} \n"
+mensaje += f"False and False -> {falso or  falso} \n\n"
+
+mensaje += "TABLA DE LA VERDAD DEL OPERADOR LOGICO NOT\n"
+mensaje += f"not True -> {not verdadero} \n"
+mensaje += f"not False -> {not falso}"
+
+print(mensaje)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
