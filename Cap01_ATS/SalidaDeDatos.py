1c5b7f1126b769e22d68deaab1202ae76141ce26
Hazael Lopez <elhazael134@gmail.com>
2020-05-13 21:59:50 -0600

    Programa que muestra tres formas distintas de imprimir texto en consola

---
+++ Cap01_ATS/SalidaDeDatos.py
@@ -0,0 +1,14 @@
+# SAlida de datos en Python
+
+'''
+    Simplemente vemos como imprimir texto en consola
+    usando tres tipos diferentes de impreciones
+    en el presente curso usaremos la tercer forma
+'''
+
+nombre = "Walter"
+edad = 23
+
+print("Nombre:" , nombre , "Edad:" , edad)
+print("Nombre: {} Edad: {}".format(nombre,edad))
+print(f"Nombre: {nombre} Edad: {edad}")
